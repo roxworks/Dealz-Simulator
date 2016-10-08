192d372a0c4ceaf118821ff0285f8b1367ec7e84
Dan Giaime <dsg6874@g.rit.edu>
2016-10-08 18:43:49 -0400

    Implemented basic phone placement

---
+++ Assets/Slot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Slot : MonoBehaviour {
+    public Color hoverColor;
+
+    private GameObject phone;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private Vector3 positionOffset = new Vector3(0,0,-1);
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+	}
+
+    void OnMouseDown() {
+        if (phone != null) {
+            Debug.Log("Can't place a phone here!");
+            return;
+        }
+
+        GameObject phoneToBuild = Buildmanager.instance.GetPhoneToPlace();
+        phone = (GameObject)Instantiate(phoneToBuild, transform.position+positionOffset, transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseEnter() {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit() {
+        rend.material.color = startColor;
+    }
+}
