759edc15695679f3e9454dec5b861fd83fe9befa
LegendRK <rohitgkaushik@gmail.com>
2016-10-09 01:12:09 -0400

    Added Camera angles; used for shop and instructions

---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menus : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LookAtMenu(Transform menuTransform) {
+        Camera.main.transform.LookAt(menuTransform.position);
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Dealz1");
+    }
+}
