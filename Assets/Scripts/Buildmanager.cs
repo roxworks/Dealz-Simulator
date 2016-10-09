b22560f3011dcd2337a5887c2dd1f7fe6e2adadc
LegendRK <rohitgkaushik@gmail.com>
2016-10-09 00:26:27 -0400

    Added new camera view; To create a store screen

---
+++ Assets/Scripts/Buildmanager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buildmanager : MonoBehaviour {
+
+    private GameObject phoneToPlace;
+    public static Buildmanager instance;
+    public GameObject standardPhonePrefab;
+
+
+	// Use this for initialization
+	void Awake () {
+        if (instance != null) { Debug.Log("More than one buildmanager!"); }
+        instance = this;
+        phoneToPlace = standardPhonePrefab;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public GameObject GetPhoneToPlace() {
+        return phoneToPlace;
+    }
+}
---
+++ Assets/Scripts/TestButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButtonClick : MonoBehaviour {
+
+	public void ClickTest ()
+	{
+		Debug.Log ("Clicked");		
+	}
+
+	public void ClickText2 (string text)
+	{
+		Debug.Log (text);
+	}
+}
